583bf966e0ec557f3809e318708a662406226943
P.A.G <64533489+Pagplayes@users.noreply.github.com>
2024-06-10 19:34:02 +0200

    #1 Grundgerüst

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.example.towerdefense {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires java.sql;
+
+
+    opens org.example.towerdefense to javafx.fxml;
+    exports org.example.game;
+}
